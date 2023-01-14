3bb4eb819336ad7c4dc0a27fc317580805895caa
Shenghao Dong <shenghaodong18@gmail.com>
2023-01-14 00:30:11 -0500

    started working on template class

---
+++ funcs.cpp
@@ -0,0 +1,19 @@
+//Make a array and if user's inputs exceed array size make a even bigger array and copy the contents over
+/*
+- size()
+- capacity()
+- empty()
+- push_back(item)
+- pop_back(n)
+- pop_back()
+- clear()
+- [] for access and assignment (assuming that given the expression a[n] that n is in range)
+*/
+
+template <typename T, int size>
+class vector{
+    public:
+
+
+    private:
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "funcs.h"
+
+int main(){
+
+    return 0;
+}
