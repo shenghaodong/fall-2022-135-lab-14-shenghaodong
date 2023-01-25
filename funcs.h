d955d6077edacea5f3bfc7594957ffc18597af5c
Shenghao Dong <shenghaodong18@gmail.com>
2023-01-24 22:27:46 -0500

    fixed makefile and added stuff to main

--- funcs.h
+++ funcs.h
@@ -1,14 +1,3 @@
-//Make a array and if user's inputs exceed array size make a even bigger array and copy the contents over
-/*
-- size()
-- capacity()
-- empty()
-- push_back(item)
-- pop_back(n)
-- pop_back()
-- clear()
-- [] for access and assignment (assuming that given the expression a[n] that n is in range)
-*/
 #pragma once
 template <class T>
 
--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main(){
     //Make Vector class
     MyVector<int> testing;
 
+    std::cout << "Should be false because the array is empty: " << testing.empty() << std::endl;
     //Fill up the class by push_back
     for(int i = 0; i < 20; i++){
         testing.push_back(i);   
     }
-    std::cout << testing.empty() << std::endl;
-    testing.clear();
-    std::cout << testing.empty() << std::endl;
-    std::cout << testing[19] << std::endl;
+    std::cout << "Added stuff to Array so it should return false now: " << testing.empty() << std::endl;
+    std::cout << "Should print 19: " << testing[19] << std::endl;
+    std::cout << "Should print 40 since the array just reached max size 20 and the array size should be doubled: " << testing.capacity() << std::endl;
     return 0;
 }
